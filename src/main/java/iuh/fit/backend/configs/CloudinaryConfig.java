7787e7f631fac0386cfbf6d67f6c21e8f83c981a
hovinhduy <hovinhduy0712@gmail.com>
2024-12-15 12:14:28 +0700

    update application

--- src/main/java/iuh/fit/backend/configs/CloudinaryConfig.java
+++ src/main/java/iuh/fit/backend/configs/CloudinaryConfig.java
@@ -1,31 +1,31 @@
-/*
- * @ (#) CloudinaryConfig.java        1.0     11/29/2024
- *
- * Copyright (c) 2024 IUH. All rights reserved.
- */
-
 package iuh.fit.backend.configs;
 
 import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.HashMap;
 import java.util.Map;
 
-/*
- * @description
- * @author: Ho Vinh Duy
- * @date:   11/29/2024
- */
 @Configuration
 public class CloudinaryConfig {
+
+    @Value("${cloudinary.cloud-name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api-key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api-secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
         Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", "dfqpclmbc");
-        config.put("api_key", "511428582334539");
-        config.put("api_secret", "Ic3wp0r8puXKxSFWCCEQqaq3n-Q");
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
         return new Cloudinary(config);
     }
 }
